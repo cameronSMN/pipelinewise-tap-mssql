359c8bcb6a6c1893ebe8e4169648a194a5a457eb
Omar Mosaad <Omar.Mohammed.Mosaad@smn.no>
2024-01-19 22:55:10 +0000

    Completely ignore c.character_maximum_length schema validation

--- tap_mssql/__init__.py
+++ tap_mssql/__init__.py
@@ -135,9 +135,9 @@ def schema_for_column(c, config):
         # https://docs.microsoft.com/en-us/sql/relational-databases/system-information-schema-views/columns-transact-sql?view=sql-server-ver15
         # -1 is not valid JSON schema
         # https://json-schema.org/understanding-json-schema/reference/string.html#length
-        if c.character_maximum_length != -1:
-            #result.maxLength = c.character_maximum_length
-            result.maxLength = 60
+        #if c.character_maximum_length != -1:
+        #    result.maxLength = c.character_maximum_length
+        #    result.maxLength = 60
 
     elif data_type in DATETIME_TYPES:
         result.additionalProperties = {"sql_data_type": data_type}
